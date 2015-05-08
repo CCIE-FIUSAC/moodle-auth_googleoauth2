0b75c0b39a68a2cd348ab230e70e13996bc3be0f
David Yzaguirre <dvdyzag@gmail.com>
2015-05-08 11:03:18 -0600

    Minor version updated

--- version.php
+++ version.php
@@ -28,5 +28,5 @@ defined('MOODLE_INTERNAL') || die();
 
 $plugin->version  = 2014060704; // only increment with +1.
 $plugin->requires = 2011070100;   // Requires Moodle 2.1 or later
-$plugin->release = '1.5 (Build: 2014060703) STABLE_25';
+$plugin->release = '1.5 (Build: 2014060704) STABLE_25';
 $plugin->maturity = MATURITY_STABLE;             // this version's maturity level
