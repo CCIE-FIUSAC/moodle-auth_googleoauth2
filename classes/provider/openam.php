b4776e5242d2ca7d4254bac35625dadcc2f76469
David Yzaguirre <dvdyzag@gmail.com>
2015-07-27 17:35:23 -0600

    OpenAM update

--- classes/provider/openam.php
+++ classes/provider/openam.php
@@ -1,12 +1,12 @@
 <?php
 
-require_once $CFG->dirroot . '/auth/googleoauth2/vendor/autoload.php';
+require_once($CFG->dirroot . '/auth/googleoauth2/vendor/autoload.php');
 
 class provideroauth2openam extends League\OAuth2\Client\Provider\OpenAM {
 
     // THE VALUES YOU WANT TO CHANGE WHEN CREATING A NEW PROVIDER.
     public $sskstyle = 'facebook';
-    public $name = 'openam'; // it must be the same as the XXXXX in the class name provideroauth2XXXXX.
+    public $name = 'openam'; // It must be the same as the XXXXX in the class name provideroauth2XXXXX.
     public $readablename = 'OpenAM';
 
     /**
@@ -43,12 +43,12 @@ class provideroauth2openam extends League\OAuth2\Client\Provider\OpenAM {
     /**
      * The html button.
      *
-     * @param $authUrl
+     * @param $authurl
      * @param $providerdisplaystyle
      * @return string
      * @throws coding_exception
      */
-    public function html_button($authUrl, $providerdisplaystyle) {
-        return googleoauth2_html_button($authUrl, $providerdisplaystyle, $this);
+    public function html_button($authurl, $providerdisplaystyle) {
+        return googleoauth2_html_button($authurl, $providerdisplaystyle, $this);
     }
 }
--- openam_redirect.php
+++ openam_redirect.php
@@ -2,5 +2,5 @@
 
 
 require('../../config.php');
-require_once $CFG->dirroot . '/auth/googleoauth2/lib.php';
+require_once($CFG->dirroot . '/auth/googleoauth2/lib.php');
 googleoauth2_provider_redirect('openam');
