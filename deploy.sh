676ae38d3d87f840230af37e66d1183a50d117c0
David Yzaguirre <dvdyzag@gmail.com>
2015-09-04 13:49:53 -0600

    Deploy script

---
+++ deploy.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# Modificar variables
+destino=/mnt/memo
+zip=auth_googleoauth2_moodle28_2015090200.zip
+
+cd ..
+cp -r moodle-auth_googleoauth2 $destino
+cd $destino
+rm $zip
+mv moodle-auth_googleoauth2 googleoauth2
+rm -rf googleoauth2/metadata googleoauth2/.git googleoauth2/.gitignore googleoauth2/*.sh
+zip -r $zip googleoauth2
+rm -rf googleoauth2
